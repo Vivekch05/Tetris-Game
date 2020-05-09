80fc8ea1d02c0fe0e94957a7cd1183f58750af46
Vivek Kumar Chaurasia <vivek.chaurasia@synapse.com>
2020-05-09 19:38:31 +0530

    Updates

---
+++ precache-manifest.46af9808a87c2f1e50532855d50c62e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effff4689a17205c60065858f6f3c7f0",
+    "url": "/tetris-game/index.html"
+  },
+  {
+    "revision": "88de3288bb1c5ac07cdd",
+    "url": "/tetris-game/static/css/main.ce22e7e3.chunk.css"
+  },
+  {
+    "revision": "e83880767ec82807a763",
+    "url": "/tetris-game/static/js/2.7529b9c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris-game/static/js/2.7529b9c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88de3288bb1c5ac07cdd",
+    "url": "/tetris-game/static/js/main.ac920e09.chunk.js"
+  },
+  {
+    "revision": "684d9775417d004b43f9",
+    "url": "/tetris-game/static/js/runtime-main.b6cd34fa.js"
+  },
+  {
+    "revision": "0a33d170c03d771061d74c5124b652ff",
+    "url": "/tetris-game/static/media/Pixel LCD-7.0a33d170.ttf"
+  },
+  {
+    "revision": "11c1f63bb6d80e8c21908ec216078473",
+    "url": "/tetris-game/static/media/bg.11c1f63b.gif"
+  }
+]);
